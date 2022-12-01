13d0419d1982cefbc6a2f7524347578dc6656ce5
Shanshan Lao <65920056+shanshanlao@users.noreply.github.com>
2022-11-30 21:19:19 -0500

    Create Largest_Olympics.py

---
+++ stratascratch/Medium/Largest_Olympics.py
@@ -0,0 +1,13 @@
+# Find the Olympics with the highest number of athletes. 
+# The Olympics game is a combination of the year and the season, and is found in the 'games' column. 
+# Output the Olympics along with the corresponding number of athletes.
+
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+olympics_athletes_events.head()
+
+df = olympics_athletes_events[['name','games']]
+
+df.groupby('games')['name'].nunique().reset_index().sort_values(by='name', ascending = False).iloc[:1]
