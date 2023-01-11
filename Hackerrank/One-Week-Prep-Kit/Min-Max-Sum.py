a45a316b14b68f4fd215b919a7733e034d6d3592
Shanshan Lao <65920056+shanshanlao@users.noreply.github.com>
2023-01-11 11:58:09 -0500

    Create Min-Max-Sum.py

---
+++ Hackerrank/One-Week-Prep-Kit/Min-Max-Sum.py
@@ -0,0 +1,16 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+def miniMaxSum(arr):
+    arr.sort()
+    print(sum(arr[:-1]),sum(arr[1:]))
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
