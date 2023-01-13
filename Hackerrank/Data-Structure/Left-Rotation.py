30fb4afc09a7410a7bd70931966f3ea0e300a053
Shanshan Lao <65920056+shanshanlao@users.noreply.github.com>
2023-01-13 12:25:38 -0500

    Update Left-Rotation.py

--- Hackerrank/Data-Structure/Left-Rotation.py
+++ Hackerrank/Data-Structure/Left-Rotation.py
@@ -27,20 +27,4 @@ def rotateLeft(d, arr):
   
     return new_arr
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    first_multiple_input = input().rstrip().split()
-
-    n = int(first_multiple_input[0])
-
-    d = int(first_multiple_input[1])
-
-    arr = list(map(int, input().rstrip().split()))
-
-    result = rotateLeft(d, arr)
-
-    fptr.write(' '.join(map(str, result)))
-    fptr.write('\n')
-
-    fptr.close()
