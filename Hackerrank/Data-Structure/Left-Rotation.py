2394e561eb8e86eec18ed140ced4878ed73edff7
Shanshan Lao <65920056+shanshanlao@users.noreply.github.com>
2023-01-13 12:26:45 -0500

    Update Left-Rotation.py

--- Hackerrank/Data-Structure/Left-Rotation.py
+++ Hackerrank/Data-Structure/Left-Rotation.py
@@ -1,19 +1,8 @@
-#!/bin/python3
 
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'rotateLeft' function below.
-#
 # The function is expected to return an INTEGER_ARRAY.
 # The function accepts following parameters:
 #  1. INTEGER d
 #  2. INTEGER_ARRAY arr
-#
 
 def rotateLeft(d, arr):
     n = len(arr)
