e97e6525933aae2bf2dd32f741046bc955e65092
Shanshan Lao <65920056+shanshanlao@users.noreply.github.com>
2023-01-13 12:18:53 -0500

    Create Left-Rotation.py

---
+++ Hackerrank/Data-Structure/Left-Rotation.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'rotateLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def rotateLeft(d, arr):
+    n = len(arr)
+    new_arr = [None] * n
+    
+    for i in range(n):
+        if i >= d:
+            new_arr[i-d] = arr[i]
+        else:
+            new_arr[n-(d-i)] = arr[i]
+  
+    return new_arr
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = rotateLeft(d, arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
