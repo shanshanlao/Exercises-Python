57c214a1ab4debd14564a125da49104ad94dc2b7
Shanshan Lao <65920056+shanshanlao@users.noreply.github.com>
2023-01-13 12:25:52 -0500

    Update Dynamic_Array.py

--- Hackerrank/Data-Structure/Dynamic_Array.py
+++ Hackerrank/Data-Structure/Dynamic_Array.py
@@ -37,23 +37,3 @@ def dynamicArray(n, queries):
         
     return answer
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    first_multiple_input = input().rstrip().split()
-
-    n = int(first_multiple_input[0])
-
-    q = int(first_multiple_input[1])
-
-    queries = []
-
-    for _ in range(q):
-        queries.append(list(map(int, input().rstrip().split())))
-
-    result = dynamicArray(n, queries)
-
-    fptr.write('\n'.join(map(str, result)))
-    fptr.write('\n')
-
-    fptr.close()
