ae96ba19580bd35c0411a1528e988c8a7a97d7d1
Shanshan Lao <65920056+shanshanlao@users.noreply.github.com>
2023-01-13 12:25:22 -0500

    Create Sparse_Arrays.py

---
+++ Hackerrank/Data-Structure/Sparse_Arrays.py
@@ -0,0 +1,18 @@
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY stringList
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(stringList, queries):
+    result = [None] * len(queries)
+    
+    for i in range(len(queries)):
+        result[i] = stringList.count(queries[i])
+
+    return result
+
