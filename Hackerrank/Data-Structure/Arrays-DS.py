41364930d0d884abb447716cf3335018f352ce37
Shanshan Lao <65920056+shanshanlao@users.noreply.github.com>
2023-01-13 12:26:06 -0500

    Update Arrays-DS.py

--- Hackerrank/Data-Structure/Arrays-DS.py
+++ Hackerrank/Data-Structure/Arrays-DS.py
@@ -1,9 +1,3 @@
-import math
-import os
-import random
-import re
-import sys
-
 
 # The function is expected to return an INTEGER_ARRAY.
 # The function accepts INTEGER_ARRAY a as parameter.
@@ -18,15 +12,3 @@ def reverseArray(a):
         new_a[i] = a[n-1-i]
     
     return new_a
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-    arr_count = int(input().strip())
-    arr = list(map(int, input().rstrip().split()))
-    
-    res = reverseArray(arr)
-
-    fptr.write(' '.join(map(str, res)))
-    fptr.write('\n')
-
-    fptr.close()
