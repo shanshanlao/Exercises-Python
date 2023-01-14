4cc7bca3da0aab3df3df7c11384e961c4876f312
Shanshan Lao <65920056+shanshanlao@users.noreply.github.com>
2023-01-14 18:17:16 -0500

    Create Day5_Queue-Using-Two-Stacks.py

---
+++ Hackerrank/One-Week-Prep-Kit/Day5_Queue-Using-Two-Stacks.py
@@ -0,0 +1,23 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+if __name__ == '__main__':
+
+    queries = []
+    for _ in range(int(input())):
+        queries.append(input())
+    
+    # print(queries)
+    
+    n = len(queries)
+    queue = []
+    for i in range(n): 
+        
+        if len(queries[i]) > 1: # meaning the type is 1 - Enqueue
+            qtype, qvalue = queries[i].split(" ")   
+            queue.append(qvalue)
+        else:
+            qtype = queries[i]
+        
+            if qtype == '2': # Dequeue
+                queue.pop(0)
+            else: # Print
+                print(queue[0])
