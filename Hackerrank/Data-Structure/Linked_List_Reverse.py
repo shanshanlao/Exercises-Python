ecff768dd32984a36695b49b5368e526ce4ddc37
Shanshan Lao <65920056+shanshanlao@users.noreply.github.com>
2023-01-14 16:53:54 -0500

    Create Linked_List_Reverse.py

---
+++ Hackerrank/Data-Structure/Linked_List_Reverse.py
@@ -0,0 +1,69 @@
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep):
+    while node:
+        print(node.data, end='')
+
+        node = node.next
+
+        if node:
+            print(sep, end='')
+
+#
+# Reverse the linked list
+#
+### MY CODE ### --------
+def reversePrint(llist):
+    if llist is None:
+        return None
+    else:
+        #reversePrint(llist.next)
+        #print(llist.data)
+    
+        before = None
+        temp = llist #head
+        after = temp.next
+    
+        while (temp):
+            after = temp.next
+            temp.next = before
+            
+            before = temp
+            temp = after
+            
+    print_singly_linked_list(before, '\n')
+    print()
+
+## Validation ##
+if __name__ == '__main__':
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        reversePrint(llist.head)
