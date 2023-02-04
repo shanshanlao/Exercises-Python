de86f9edbb4432b3dc2952295be41258d92ef485
Shanshan Lao <65920056+shanshanlao@users.noreply.github.com>
2023-02-04 00:28:35 -0500

    Update 137_Single_Number.py

--- leetcode/137_Single_Number.py
+++ leetcode/137_Single_Number.py
@@ -4,6 +4,14 @@ Given a non-empty array of integers nums, every element appears twice except for
 You must implement a solution with a linear runtime complexity and use only constant extra space.
 '''
 
+# XOR
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        return sum(nums) - (sum(nums) - sum(set(nums)))*2
+        
+        xor = 0
+        for num in nums:
+            xor ^= num
+        return xor
+        
+        # not using constant extra space ?
+        # return sum(nums) - (sum(nums) - sum(set(nums)))*2
